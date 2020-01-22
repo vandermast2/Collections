8ce201cdec2e6d8e54daa8b85709f5dce572f147
sergey <sergey.k@adviscent.com>
2020-01-22 17:59:47 +0200

    add new method print HAshMap

--- app/src/main/java/com/example/collectionsexample/MainActivity.java
+++ app/src/main/java/com/example/collectionsexample/MainActivity.java
@@ -9,7 +9,7 @@ import java.util.HashMap;
 
 public class MainActivity extends AppCompatActivity {
     private ArrayList<Cat> cats;
-    private HashMap<Integer,Cat> map;
+    private HashMap<Integer, Cat> map;
 
 
     @Override
@@ -24,16 +24,22 @@ public class MainActivity extends AppCompatActivity {
     private ArrayList<Cat> catArrayList() {
         ArrayList cats = new ArrayList();
         for (int i = 0; i < 10; i++) {
-            cats.add(new Cat("Simon",(int) (Math.random() * 10)));
+            cats.add(new Cat("Simon", (int) (Math.random() * 10)));
         }
         return cats;
     }
 
-    private HashMap<Integer,Cat> catHashMap(){
-        HashMap<Integer,Cat> mapCats = new HashMap<>();
-        for (int i=0; i<10; i++){
-            mapCats.put(i,new Cat("Mike",(int) (Math.random() * 10)));
+    private HashMap<Integer, Cat> catHashMap() {
+        HashMap<Integer, Cat> mapCats = new HashMap<>();
+        for (int i = 0; i < 10; i++) {
+            mapCats.put(i, new Cat("Mike", (int) (Math.random() * 10)));
         }
         return mapCats;
+    }
+
+    private void print(HashMap<Integer, Cat> cats) {
+        for (int i = 0; i < cats.size(); i++) {
+            System.out.println("Cat name:" + cats.get(i).getName());
+        }
     }
 }
