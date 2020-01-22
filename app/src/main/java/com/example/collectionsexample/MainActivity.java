d13ab72718ca3690f1b0673f1625605cd12a46d7
sergey <sergey.k@adviscent.com>
2020-01-22 17:02:24 +0200

    first commit

---
+++ app/src/main/java/com/example/collectionsexample/MainActivity.java
@@ -0,0 +1,39 @@
+package com.example.collectionsexample;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class MainActivity extends AppCompatActivity {
+    private ArrayList<Cat> cats;
+    private HashMap<Integer,Cat> map;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        cats = catArrayList();
+        map = catHashMap();
+
+    }
+
+    private ArrayList<Cat> catArrayList() {
+        ArrayList cats = new ArrayList();
+        for (int i = 0; i < 10; i++) {
+            cats.add(new Cat("Simon",(int) (Math.random() * 10)));
+        }
+        return cats;
+    }
+
+    private HashMap<Integer,Cat> catHashMap(){
+        HashMap<Integer,Cat> mapCats = new HashMap<>();
+        for (int i=0; i<10; i++){
+            mapCats.put(i,new Cat("Mike",(int) (Math.random() * 10)));
+        }
+        return mapCats;
+    }
+}
